d3a8379d39f2209e9f7c1230aec9572bf68f582b
Dennis de Greef <github@link0.net>
2016-05-12 15:03:08 +0200

    Add bottle

--- qtkeychain.rb
+++ qtkeychain.rb
@@ -6,11 +6,16 @@ class Qtkeychain < Formula
   homepage 'https://github.com/frankosterfeld/qtkeychain'
   head 'https://github.com/frankosterfeld/qtkeychain.git', :using => :git
   url 'https://github.com/frankosterfeld/qtkeychain/archive/v0.6.2.tar.gz'
-  #sha1 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
   sha256 'ae13459234feeeab3a154457319d9b26ee9600973443517c77e055838ebae63c'
 
   depends_on 'cmake' => :build
   # depends on Qt, but we want to accept a system Qt as well. How?
+
+  bottle do
+    cellar :any
+    root_url "https://link0.net/homebrew"
+    sha256 "817ef5e3ae0b6b6624dc97f6930e048ab90edce9dee79e150a116e4fb9800865" => :mavericks
+  end
 
   def install
 
